8da323b8749b57afec6a4761b7461d41803b93b1
tiluu <tina.p.luu@gmail.com>
2013-09-13 16:40:19 -0700

    add items to cart

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,16 +1,33 @@
-class shop_items(object):
-	def _init_(self, name, total, number_items):
-		self.name = name
-		self.total = total
-		self.number_items = number_items
-
-	def addTotalAmount(self, total, number):
-		totalAmount = total*number
-		return totalAmount
-		
-
-class shopping_cart(shop_items):
-	pass
+class Cart(object):
+	items_in_cart = {}
+	total = 0
+	def __init__(self, customer_name):
+		self.customer_name = customer_name
 	
+	def grandTotal(self):
+		for i in self.items_in_cart:
+			self.total += self.items_in_cart[i]
+		print "The total is %s" %(str(self.total))
 
-		
+class Items(Cart):
+	def __init__(self, item, price):
+		self.items = item
+		self.price = price
+	def addItem(self):
+		self.items_in_cart[self.items] = self.price
+		print "Added %s in cart" %(self.items)
+
+my_cart = Cart('tina')
+things = Items('tina', 3)
+things.addItem()
+things2 = Items('banana', 3)
+things2.addItem()
+my_cart.grandTotal()
+
+
+
+
+
+
+
+
--- test_shopping-cart.py
+++ test_shopping-cart.py
@@ -1,19 +1,29 @@
 import unittest
 
-from shopping_cart import shopping_cart, shop_items
+from shopping_cart import Cart, Items
 
 class test_shop_items(unittest.TestCase):
 
 	def setUp(self):
-		self.shopItems= shop_items()
+		self.shopItems = Items('bananasdfa',3)
+		
 		self.name ='banana'
 		self.total = 5.2
-		self.number = 2
 
-	def test_item_final_amount(self):
-		grandTotal = self.shopItems.addTotalAmount(self.total, self.number)
-		self.assertEqual(10.4, grandTotal) 
+	def test_add_item_to_cart(self):
+		self.shopItems.addItem()
+		print self.shopItems.items_in_cart
+		self.assertIsInstance(self.name, self.shopItems.items_in_cart)
+	#def test_item_final_amount(self):
+	#	grandTotal = self.shopItems.addTotalAmount(self.total, self.number)
+	#	self.assertEqual(10.4, grandTotal) 
 
-	def test_item_is_string(self):
-		self.assertIsInstance(self.name, str)
+	#def test_item_is_string(self):
+	#	self.assertIsInstance(self.name, str)
+
+	#class test_shopping_cart(unittest.TestCase):
+	#def test_add_items_to_cart(self):
+
+
+
 
