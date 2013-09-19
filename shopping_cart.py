535eb0e784b5735657e022813ff372db6ba0e628
tiluu <tina.p.luu@gmail.com>
2013-09-19 13:13:50 -0700

    updated with proper classes

--- shopping_cart.py
+++ shopping_cart.py
@@ -9,20 +9,40 @@ class Cart(object):
 			self.total += self.items_in_cart[i]
 		print "The total is %s" %(str(self.total))
 
-class Items(Cart):
+	def addItem(self, items,price):
+		self.items_in_cart[items] = price
+		print "Added %s in cart" %(items)
+
+	def clearCart(self):
+		self.items_in_cart = {}
+		print self.items_in_cart
+
+class Items(object):
 	def __init__(self, item, price):
 		self.items = item
 		self.price = price
-	def addItem(self):
-		self.items_in_cart[self.items] = self.price
-		print "Added %s in cart" %(self.items)
 
-my_cart = Cart('tina')
-things = Items('tina', 3)
-things.addItem()
-things2 = Items('banana', 3)
-things2.addItem()
-my_cart.grandTotal()
+class Fruit(Items):
+	IamAFruit = True
+
+class Vegetables(Items):
+	IamAVeggie = True
+
+print Cart.items_in_cart
+
+banana = Fruit('banana', 2)
+print banana.items
+add_to_cart = Cart('tina')
+print add_to_cart.addItem(banana.items, banana.price)
+print "print cart", add_to_cart.items_in_cart
+
+cucumber = Vegetables('cucumber', 3)
+print add_to_cart.addItem(cucumber.items, cucumber.price)
+print "print cart", add_to_cart.items_in_cart
+
+add_to_cart.grandTotal()
+	
+
 
 
 
